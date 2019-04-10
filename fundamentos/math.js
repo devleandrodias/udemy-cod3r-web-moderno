b413ecdb31fb115fc7add72c673dace4d411e8db
SMARAPD <leandro.dias_profissional@outlook.com.br>
2019-04-10 14:12:13 -0300

    Web Moderno

---
+++ fundamentos/math.js
@@ -0,0 +1,7 @@
+//Calcular raio de uma circunferência
+
+const raio = 5.6;
+const area = Math.PI * Math.pow(raio, 2);
+
+console.log(area);
+console.log(typeof(Math));
---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,14 @@
+//Resulta em Infinity
+console.log(7 / 0); 
+
+//Isso não da certo
+console.log("Show!" * 2);
+
+//Se oq tiver em uma string for um número acontece a divisão
+console.log("10" / 2); 
+
+//Cuidado há impressiões em relação a ponto flutuante
+console.log(0.1 + 0.7);
+
+//Tofixed limita campos após a virgula em pontos flutuantes
+console.log((10.50).toFixed(1));
