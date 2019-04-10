00b6a8853524df291caaf73ec405cc6085dc78d8
DevLendroDias <leandro.dias_profissional@outlook.com.br>
2019-04-09 22:17:22 -0300

    Web Moderno

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,5 @@
+console.log('Senteça de código'); //Isso é uma sentença de código
+
+console.log('Hello Word'); //Isso é um comentário
+
+//O java script é organizado em sentenças de código e blocos de código 
